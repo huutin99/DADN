b8478d40cee53015f5e435bc74f383771d6863a3
Tin <maihuynhhuutin@gmail.com>
2020-05-16 00:24:24 +0700

    Route for Login, Signup

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import *
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/login')
 def login():
     return render_template('login.html')
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
-
-if __name__ == '__main__':
-    app.run()
