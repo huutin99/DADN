8d0664846f9314dc5bcae8d306691ba90b7d965e
Tin <maihuynhhuutin@gmail.com>
2020-05-21 09:39:03 +0700

    Complete login

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
 import re
 
-from flask import Flask, session, redirect, url_for, request, render_template
+from flask import Flask, session, redirect, url_for, request, render_template, flash, session
 from flask_session import Session
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import scoped_session, sessionmaker, Session
+from datetime import timedelta
 from models import *
 
 app = Flask(__name__)
+app.secret_key = b'123456789'
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
@@ -20,6 +22,7 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(weeks=1)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
@@ -28,6 +31,8 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
+    if "username" in session:
+        return redirect(url_for("dashboard"))
     return redirect(url_for("login"))
 
 
@@ -39,6 +44,8 @@ def login():
         pwd = request.form["pwd"]
         user = db.query(User).filter(User.name==username, User.pwd==pwd).all()
         if user:
+            session['logged_in'] = True
+            session['username'] = username
             return redirect(url_for("dashboard"))
         else:
             error = "*Invalid credential, please try again."
@@ -47,7 +54,6 @@ def login():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    succ = ""
     if request.method == "POST":
         username = request.form["username"]
         email = request.form["email"]
@@ -60,10 +66,11 @@ def signup():
             user = User(id=id, name=username, mail=email, pwd=pwd, admin=False)
             db.add(user)
             db.commit()
-            succ = "Successful registered."
-    return render_template("signup.html", succ=succ)
+    return render_template("signup.html", succ="Successful registered.")
 
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    if "logged_in" in session:
+        return render_template("dashboard.html")
+    return redirect(url_for("login"))
